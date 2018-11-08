fa519241a48a8d0ece2ccac330131a7e314199d4
Elisabeth Kirkebø <Elisabeth@Elisabeths-MacBook-Pro-2.local>
2018-11-08 19:15:01 +0100

    dette skal ikke pushes

---
+++ doctor-diary/src/Hei.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Hei extends Component {
+
+  render() {
+    return (
+      <div className="Hei">
+        <header className="Hei-header">
+          <img src={logo} className="Hei-logo" alt="logo" />
+          <p>
+            Hei!
+          </p>
+        </header>
+      </div>
+    );
+  }
+
+
+}
