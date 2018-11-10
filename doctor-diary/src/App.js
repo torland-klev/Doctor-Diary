6ba53a56798e320858897b395095029dbde9d940
torland-klev <torland-klev@users.noreply.github.com>
2018-11-10 17:00:04 +0100

    Added the views Declined, New and Pending for the Doctor

--- doctor-diary/src/App.js
+++ doctor-diary/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Home from './Views/Home.js'
 import DoctorHome from './Views/DoctorHome.js'
+import New from './Views/New.js';
+import Pending from './Views/Pending.js';
+import Declined from './Views/Declined.js';
 import HealthOfficerHome from './Views/HealthOfficerHome.js'
 import {BrowserRouter, Route} from 'react-router-dom';
 
@@ -10,9 +13,16 @@ class App extends Component {
       //<Browse Router> brukes for å linke til andre sider til applikasjonen, må importeres over
       <BrowserRouter>
         <div>
+
+          {/* Routing from main menu */}
           <Route exact={true} path='/' render={() => (<div className="App"> <Home /> </div>)} />
           <Route exact={true} path='/healthofficer' render={() => (<div className="App"> <HealthOfficerHome /></div>)} />
           <Route exact={true} path='/doctor' render={() => (<div className="App"> <DoctorHome /> </div>)} />
+
+          {/* Routing from the doctor side to the doctors reports */}
+          <Route exact={true} path='/doctor/pending' render={() => (<div className="App"> <Pending /> </div>)} />
+          <Route exact={true} path='/doctor/new' render={() => (<div className="App"> <New /></div>)} />
+          <Route exact={true} path='/doctor/declined' render={() => (<div className="App"> <Declined /> </div>)} />
         </div>
       </BrowserRouter>
     );
--- doctor-diary/src/Views/DoctorHome.js
+++ doctor-diary/src/Views/DoctorHome.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 export default class DoctorHome extends Component {
     state = {
@@ -6,14 +7,31 @@ export default class DoctorHome extends Component {
     }
     render () {
         return (
-            <div>
-                <header>
-                <h1>Doctor Diary</h1>
-                    <button>
-                        <a className='Home-link' href='/'>Go back</a>
-                    </button>
+          <div className="wrapper">
+
+            <header>
+              <h1>Doctor Diary</h1>
+                <button>
+                  <a className='Home-link' href='/'>Go back</a>
+                </button>
+                <button>
+                    <a className="Home-link" href='doctor/pending'>
+                    <h1>Pending</h1>
+                    </a>
+                </button>
+                <button>
+                    <a className="Home-link" href='doctor/new'>
+                    <h1>New</h1>
+                    </a>
+                </button>
+                <button>
+                    <a className="Home-link" href='doctor/declined'>
+                    <h1>Declined</h1>
+                    </a>
+                </button>
             </header>
-            </div>
+
+          </div>
         )
     }
 }
