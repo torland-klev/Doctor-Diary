aaa8cce05f0ecd7f86ec52441404ee4587127381
Tegelaár <linneak@student.matnat.uio.no>
2018-11-10 17:36:03 +0100

    merge conflicts fixes

--- doctor-diary/src/App.js
+++ doctor-diary/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Home from './Views/Home.js'
 import DoctorHome from './Views/DoctorHome.js'
-import New from './Views/New.js';
 import Pending from './Views/Pending.js';
 import Declined from './Views/Declined.js';
 import HealthOfficerHome from './Views/HealthOfficerHome.js'
@@ -19,14 +18,11 @@ class App extends Component {
           <Route exact={true} path='/' render={() => (<div className="App"> <Home /> </div>)} />
           <Route exact={true} path='/healthofficer' render={() => (<div className="App"> <HealthOfficerHome /></div>)} />
           <Route exact={true} path='/doctor' render={() => (<div className="App"> <DoctorHome /> </div>)} />
-<<<<<<< HEAD
 
           {/* Routing from the doctor side to the doctors reports */}
           <Route exact={true} path='/doctor/pending' render={() => (<div className="App"> <Pending /> </div>)} />
           <Route exact={true} path='/doctor/declined' render={() => (<div className="App"> <Declined /> </div>)} />
-=======
-          <Route exact={true} path='/newEntry' render={() => (<div className="App"> <NewEntry /> </div>)} />
->>>>>>> 1fa0fc89404f9d1b02d7622a1725aae96e87fe77
+          <Route exact={true} path='/doctor/newEntry' render={() => (<div className="App"> <NewEntry /> </div>)} />
         </div>
       </BrowserRouter>
     );
--- doctor-diary/src/Views/DoctorHome.js
+++ doctor-diary/src/Views/DoctorHome.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
 import Header from '../Components/Header.js';
 
 
@@ -8,46 +7,16 @@ export default class DoctorHome extends Component {
 
     }
     render () {
-<<<<<<< HEAD
         return (
-          <div className="wrapper">
-            <header>
-              <h1>Doctor Diary</h1>
-                <button>
-                    <a className="Home-link" href='doctor/pending'>
-                    <h1>Pending</h1>
-                    </a>
-                </button>
-                <button>
-                    <a className="NewEntry" href='doctor/newEntry'>
-                    <h1>New Entry</h1>
-                    </a>
-                </button>
-                <button>
-                    <a className="Home-link" href='doctor/declined'>
-                    <h1>Declined</h1>
-                    </a>
-                </button>
-                <div className="Home">
-                    <Header />
-                    <header>
-                    <h1>Doctor Diary</h1>
-                        <button>
-                            <a href='/'>Go back</a>
-                        </button>
-                    </header>
-                </div>
-              </header>
-=======
-        return(
             <div className="Home">
                 <Header />
                 <main className="Home-main">
-                    <h1>Doctor Diary</h1>
-                    <a href='newEntry' className="Home-button">New entry</a>
+                <h1>Doctor Diary</h1>
+                    <a href='doctor/pending' className="Home-button">Pending</a>
+                    <a href='doctor/declined' className="Home-button">Declined</a>
+                    <a href='doctor/newEntry' className="Home-button">New entry</a>
                     <a href='/' className="Home-button">Go back</a>
                 </main>
->>>>>>> f7b1b7d6fc46b56cc6678f2ccde28ec9d39d5261
             </div>
           )
     }
